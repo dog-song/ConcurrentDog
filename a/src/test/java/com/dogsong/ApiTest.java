5296d3d848f7a0031b6a21614e7eb3ff2c51bf6c
domi.song <domi.song@yunzhihui.com>
2021-12-01 17:05:06 +0800

    HashMap 扩容因子Test

--- a/src/test/java/com/dogsong/ApiTest.java
+++ a/src/test/java/com/dogsong/ApiTest.java
@@ -4,7 +4,9 @@ import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Test
@@ -45,6 +47,33 @@ public class ApiTest {
 
         // 输出测试结果
         System.out.println("测试结果：" + JSON.toJSONString(tab));
+    }
+
+    @Test
+    public void test_hashMap() {
+        List<String> list = new ArrayList<>();
+        list.add("jlkk");
+        list.add("lopi");
+        list.add("jmdw");
+        list.add("e4we");
+        list.add("io98");
+        list.add("nmhg");
+        list.add("vfg6");
+        list.add("gfrt");
+        list.add("alpo");
+        list.add("vfbh");
+        list.add("bnhj");
+        list.add("zuio");
+        list.add("iu8e");
+        list.add("yhjk");
+        list.add("plop");
+        list.add("dd0p");
+
+        for (String key : list) {
+            int hash = key.hashCode() ^ (key.hashCode() >>> 16);
+            System.out.println("字符串：" + key + " \tIdx(16)：" + ((16 - 1) & hash) + " \tBit值：" + Integer.toBinaryString(hash) + " - " + Integer.toBinaryString(hash & 16) + " \t\tIdx(32)：" + ((32 - 1) & hash));
+            System.out.println(Integer.toBinaryString(key.hashCode()) +" "+ Integer.toBinaryString(hash) + " " + Integer.toBinaryString((32 - 1) & hash));
+        }
     }
 
 }
