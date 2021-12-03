2494742731924411999a72c10c27240c0993a418
domi.song <domi.song@yunzhihui.com>
2021-12-03 14:10:12 +0800

    [203]移除链表元素-复杂度分析

--- a/src/main/java/com/dogsong/a/Solution_203.java
+++ a/src/main/java/com/dogsong/a/Solution_203.java
@@ -18,6 +18,9 @@ package com.dogsong.a;
  */
 public class Solution_203 {
 
+    /**
+     * 时间复杂度 O(N)，空间复杂度 O(1)
+     */
     public ListNode removeElements(ListNode head, int val) {
         // 生成一个新链表
         ListNode dummy = new ListNode();
