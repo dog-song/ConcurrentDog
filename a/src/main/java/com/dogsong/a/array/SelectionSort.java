bde26f6efeda410245e5a024c57be5e25f5e5bdf
dogsong <dogsong99@gmail.com>
2022-03-30 08:33:44 +0800

    选择排序

---
+++ a/src/main/java/com/dogsong/a/array/SelectionSort.java
@@ -0,0 +1,36 @@
+package com.dogsong.a.array;
+
+/**
+ * SelectionSort 不稳定
+ *
+ * @author <a href="mailto:dogsong99@gmail.com">dogsong</a>
+ * @since 2022/3/30
+ */
+public class SelectionSort {
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+   private static int maxIndex(int[] A, int i, int j) {
+        int max = Integer.MIN_VALUE;
+        int maxIndex = i;
+
+        for (int k = 0; k < j; k++) {
+           if (max < A[k]) {
+               max = A[k];
+               maxIndex = k;
+           }
+        }
+        return maxIndex;
+   }
+
+   public static void sort(int[] A) {
+       for (int i = A.length - 1; i >= 0; i--) {
+           int j = maxIndex(A, 0, i+1);
+           swap(A, i, j);
+       }
+   }
+}
