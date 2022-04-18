50e485a8c02dd904f0b88ff66dc43e703d672a05
dogsong <dogsong99@gmail.com>
2022-04-18 17:05:37 +0800

    Stream.peek 作用

--- a/src/main/java/com/dogsong/b/hashmap/Demo.java
+++ a/src/main/java/com/dogsong/b/hashmap/Demo.java
@@ -14,7 +14,14 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        Map<String, String> map = new HashMap<>(2);
+        Map<String, String> map = new HashMap<>();
+
+        String put = map.put("zhangsan", "zhangsan");
+        System.out.println(put);
+
+
+        String put1 = map.put("zhangsan", "lisi");
+        System.out.println(put1);
     }
 
 }
