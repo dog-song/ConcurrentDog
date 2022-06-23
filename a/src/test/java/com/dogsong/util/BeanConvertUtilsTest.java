8ca74fc12ed001d7cea6825f645af9d9e75198f8
dogsong <dogsong99@gmail.com>
2022-06-23 23:51:42 +0800

    fixed 注释

--- a/src/test/java/com/dogsong/util/BeanConvertUtilsTest.java
+++ a/src/test/java/com/dogsong/util/BeanConvertUtilsTest.java
@@ -10,8 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.BeanUtils;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 
 /**
